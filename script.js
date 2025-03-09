c883d3afe7dcfdf8c3428f48f080161f6dcb1ad3
Abhishek sahu <153366397+abhisheksahu1412@users.noreply.github.com>
2025-03-09 09:52:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+document.getElementById("birthday-message").innerText = "Happy Birthday to my wonderful brother! Wishing you a day filled with love, joy, and all your favorite things. Enjoy your special day!";
